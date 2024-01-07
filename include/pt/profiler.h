465c67e3a9ddd2af54cc1d577af847b380a30949
Peter Toth <peter.t.toth92@gmail.com>
2024-01-07 22:21:03 +0100

    added Profiler class defined a new macro for compilation   'PT_MEASURE_PERFORMANCE'

---
+++ include/pt/profiler.h
@@ -0,0 +1,67 @@
+/*
+ * -------------------------------------------------------------------------
+ * FILE:    profiler.h
+ * AUTHOR:  Peter Toth
+ * E-MAIL:  peter.t.toth92@gmail.com
+ * PURPOSE: Performance profiler class.
+ *            Constructor starts the measurement.
+ *            Destructor stops and records the measurement.
+ *          If 'PT_MEASURE_PERFORMANCE' is not defined, the profiling macros have zero overhead.
+ * USAGE:   PERF_WATCH( "foo_measurement", foo( param1, param2, ... ) )
+ * -------------------------------------------------------------------------
+ */
+
+#include "pt/name.h"
+
+#include <chrono>
+#include <string>
+
+
+namespace pt{
+
+class PerfProfiler
+{
+public:
+    using Timestamp = std::chrono::time_point< std::chrono::steady_clock >;
+
+    struct Measurement{
+        std::string name;
+        Timestamp startTime;
+        Timestamp endTime;
+
+        Measurement()
+        {}
+        Measurement( const std::string& name_, Timestamp startTime_, Timestamp endTime_ ):
+            name( name_ ), startTime( startTime_ ), endTime( endTime_ )
+        {}
+    };
+
+    PerfProfiler( char* id );
+    PerfProfiler( const char* const id );
+    PerfProfiler( const std::string& id );
+    PerfProfiler( const pt::Name& id );
+    PerfProfiler( const PerfProfiler& other ) = delete;
+    PerfProfiler( PerfProfiler&& other ) = delete;
+    virtual ~PerfProfiler();
+    PerfProfiler& operator=( const PerfProfiler& other ) = delete;
+    PerfProfiler& operator=( PerfProfiler&& other ) = delete;
+
+private:
+    std::string mName;
+    Timestamp   mStartTime;
+
+};
+
+} // end of namespace 'pt'
+
+
+#ifdef PT_MEASURE_PERFORMANCE
+    #define PERF_WATCH( PARAM_PERF_ID, PARAM_FUNCTION_CALL ) \
+    { \
+        pt::PerfProfiler perf_watcher( PARAM_PERF_ID ); \
+        PARAM_FUNCTION_CALL; \
+    }
+#else
+    #define PERF_WATCH( PARAM_PERF_ID, PARAM_FUNCTION_CALL ) PARAM_FUNCTION_CALL
+#endif
+
---
+++ src/pt/profiler.cpp
@@ -0,0 +1,54 @@
+#include "pt/profiler.h"
+
+#include "pt/alias.h"
+
+#include <assert.h>
+#include <vector>
+
+//TODO: remove this
+#include <iostream>
+
+//TODO: make the vector double-buffered (fast recording, slow file writing)
+//TODO: empty the vector to some output (can be a graph view widget or a file)
+
+std::mutex gmMeasurementsMutex;
+std::vector< pt::PerfProfiler::Measurement > gmMeasurements = std::vector< pt::PerfProfiler::Measurement >();
+
+
+
+pt::PerfProfiler::
+PerfProfiler( char* id ):
+    mName( id ), mStartTime( std::chrono::steady_clock::now() )
+{}
+
+pt::PerfProfiler::
+PerfProfiler( const char* const id ):
+    mName( id ), mStartTime( std::chrono::steady_clock::now() )
+{}
+
+pt::PerfProfiler::
+PerfProfiler( const std::string& id ):
+    mName( id ), mStartTime( std::chrono::steady_clock::now() )
+{}
+
+pt::PerfProfiler::
+PerfProfiler( const Name& id ):
+    mName( id.GetStdString() ), mStartTime( std::chrono::steady_clock::now() )
+{}
+
+
+pt::PerfProfiler::
+~PerfProfiler()
+{
+    Timestamp endTime = std::chrono::steady_clock::now();
+    // TODO: use this
+    //pt::MutexLockGuard lock( gmMeasurementsMutex );
+    //gmMeasurements.push_back( Measurement( std::move( mName ), mStartTime, endTime ) );
+    //  do not use mId from here on (it was moved)
+    //-----
+    // TODO: remove this
+    //temporary reroute to console log until multithreaded execution is implemented
+    auto duration_us = std::chrono::duration_cast< std::chrono::microseconds >( endTime - mStartTime );
+    const std::chrono::duration<long, std::micro> usec = duration_us;
+    std::cout << "Perf measurement: '" << mName << "': " << usec.count() << "us\n";
+}
