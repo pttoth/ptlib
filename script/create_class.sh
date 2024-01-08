f7cf9028e60904e97c94f99ea2818f63717038ec
Peter Toth <peter.t.toth92@gmail.com>
2024-01-08 21:20:19 +0100

    'script/create_class.sh'   added Const pointer class aliases   added Unique pointer class aliases   Weak pointer aliases' are now named '...WPtr' instead of '...PtrW'   threw out unnecessary dir creation code

--- script/create_class.sh
+++ script/create_class.sh
@@ -27,80 +27,6 @@ sFilePathS=""
 #  function definitions
 #--------------------------------------------------
 
-
-#fnReverseArray
-#  param1: array to reverse
-#  param2: output array
-fnReverseArray()
-{
-	#check parameter count
-	if [[ "$#" != 2 ]];
-	then
-		echo "error: invalid argument count (should be 2)"
-		echo "  param1: array to reverse"
-		echo "  param2: output array"
-		return 1
-	fi
-
-	declare -n arr="$1" rev="$2"
-    for i in "${arr[@]}"
-    do
-        rev=("$i" "${rev[@]}")
-    done
-}
-
-
-#fnCreateSubdirectory
-#  param1: root directory path
-#  param2: path inside root directory
-fnCreateSubdirectory ()
-{
-	#check parameter count
-	if [[ "$#" < 2 ]];
-	then
-		echo "error: too few arguments"
-		return 1
-	elif [[ "$#" > 2 ]];
-	then
-		echo "error: too many arguments"
-		return 1
-	fi
-
-	#define locals
-	local sPathRoot=$1
-	local sPathRel=$2
-	local sCurrent=$sPathRel
-	local sArrPaths=()
-	local sArrPathsRev=()
-	local bDone=0
-	local iCount=0
-
-	#find the deepest folder that already exists
-	#  and collect each, that doesn't
-	bDone=0
-	while [[ $bDone == 0 ]];
-	do
-		#if path doesn't exist
-		if [[ 0 != $(test -e $sCurrent) ]] && [[ $sCurrent != "" ]] && [[ $sCurrent != '.' ]] && [[ $sCurrent != '/' ]];
-		then
-			sArrPaths+=(${sCurrent})
-			sCurrent=$(dirname $sCurrent)
-			iCount=$((iCount + 1))
-		else
-			bDone=1
-		fi
-	done
-
-	#reverse the array of paths to get correct order
-	fnReverseArray sArrPaths sArrPathsRev
-
-	#create missing folders
-	mkdir ${sArrPathsRev[@]} &> /dev/null
-
-	return 0
-}
-
-
 fnReadClassName()#CFG_START
 {
 	local sInBuffer=""
@@ -408,9 +334,8 @@ echo "--------------------------------------------------"
 #  create file
 #--------------------------------------------------
 
-#TODO: fix, second param should be relative
-fnCreateSubdirectory $(dirname $scriptdir) $(dirname $sAbsPathH)
-fnCreateSubdirectory $(dirname $scriptdir) $(dirname $sAbsPathS)
+mkdir -p $(dirname $sAbsPathH)
+mkdir -p $(dirname $sAbsPathS)
 
 #TODO: check again for existing files and silently rename them to <name>.bak, just in case someone created them since checking
 
