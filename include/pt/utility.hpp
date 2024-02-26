4f7a3625c630ab2cf5e725fea549d96cc7f76a8b
Peter Toth <peter.t.toth92@gmail.com>
2024-02-26 01:45:06 +0100

    added 'PT_FORWARD_DECLARE_CLASS(ClassName)' macro

--- include/pt/utility.hpp
+++ include/pt/utility.hpp
@@ -23,10 +23,19 @@
 #define PT_UNIMPLEMENTED_FUNCTION \
 throw std::logic_error( "Called function is not implemented" );
 
+#define PT_FORWARD_DECLARE_CLASS( CLASSNAME ) \
+class CLASSNAME; \
+using CLASSNAME##Ptr          = std::shared_ptr< CLASSNAME >; \
+using Const##CLASSNAME##Ptr   = std::shared_ptr< const CLASSNAME >; \
+using CLASSNAME##WPtr         = std::weak_ptr< CLASSNAME >; \
+using Const##CLASSNAME##WPtr  = std::weak_ptr< const CLASSNAME >; \
+using CLASSNAME##UPtr         = std::unique_ptr< CLASSNAME >; \
+using Const##CLASSNAME##UPtr  = std::unique_ptr< const CLASSNAME >;
+
+
 
 //return the enum name in parameter as string
 #define EnumToString(ENUM_NAME) #ENUM_NAME
-
 
 
 // alias for shortening 'std::make_shared' calls
