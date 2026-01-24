c0aa76cf995cef7699a05876841247c9e1968ea3
Peter Toth <peter.t.toth92@gmail.com>
2026-01-24 02:39:22 +0100

    alias   refactored i8...i64 to s8...s64

--- include/pt/alias.h
+++ include/pt/alias.h
@@ -28,10 +28,10 @@ using ConstStdStringWPtr  = std::weak_ptr< const std::string >;
 using StdStringUPtr       = std::unique_ptr< std::string >;
 using ConstStdStringUPtr  = std::unique_ptr< const std::string >;
 
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
+using s8  = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
 
 using u8  = uint8_t;
 using u16 = uint16_t;
